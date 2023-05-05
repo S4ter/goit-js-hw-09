62de99858827ac96fdbb8b8905ccefe57c8cf651
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-05-06 00:01:40 +0200

    ad 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
-
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let changeColor = null;
+startBtn.addEventListener('click', () => {
+  changeColor = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(changeColor);
+  startBtn.removeAttribute('disabled', 'disabled');
+});
