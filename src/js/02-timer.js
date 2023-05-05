743f1732dcbf8a77304896dc8fb020afe4724539
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-05-06 00:37:54 +0200

    some changes to ad 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,46 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+const startBtn = document.querySelector('[data-start');
+const calendar = document.querySelector('#datetime-picker');
+
+const currentDate = new Date();
+console.log(currentDate);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  locale: {
+    firstDayOfWeek: 1,
+  },
+  onClose(selectedDates) {
+    if (selectedDates[0] > new Date()) {
+      Notiflix.Notify.success('Press start');
+      startBtn.disabled = false;
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    }
+  },
+};
+let date = flatpickr(calendar, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
