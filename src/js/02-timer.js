9afed6965eb8a29d4f86724dcb62922e6e392dcd
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-05-06 13:45:18 +0200

    ad 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-const startBtn = document.querySelector('[data-start');
+const startBtn = document.querySelector('[data-start]');
 const calendar = document.querySelector('#datetime-picker');
+const daysCount = document.querySelector('[data-days]');
+const hoursCount = document.querySelector('[data-hours]');
+const minutesCount = document.querySelector('[data-minutes]');
+const secondsCount = document.querySelector('[data-seconds]');
 
-const currentDate = new Date();
-console.log(currentDate);
+let countdown;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -15,32 +19,53 @@ const options = {
     firstDayOfWeek: 1,
   },
   onClose(selectedDates) {
-    if (selectedDates[0] > new Date()) {
-      Notiflix.Notify.success('Press start');
-      startBtn.disabled = false;
-    } else {
+    if (selectedDates[0] < new Date()) {
       Notiflix.Notify.failure('Please choose a date in the future');
       startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
     }
   },
 };
+
 let date = flatpickr(calendar, options);
 
+function countTime() {
+  const selectedDate = new Date(date.selectedDates[0]);
+
+  startBtn.disabled = true;
+  clearInterval(countdown);
+
+  countdown = setInterval(() => {
+    const timeLeft = selectedDate - new Date();
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    daysCount.textContent = days.toString().padStart(2, '0');
+    hoursCount.textContent = hours.toString().padStart(2, '0');
+    minutesCount.textContent = minutes.toString().padStart(2, '0');
+    secondsCount.textContent = seconds.toString().padStart(2, '0');
+
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      startBtn.disabled = false;
+      Notiflix.Notify.success('Countdown finished');
+      daysCount.textContent = '00';
+      hoursCount.textContent = '00';
+      minutesCount.textContent = '00';
+      secondsCount.textContent = '00';
+    }
+  }, 1000);
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
+startBtn.addEventListener('click', countTime);
