911b373bbc0bbcfe1e818a62c0416bf41f89e4e5
S4ter <119447102+S4ter@users.noreply.github.com>
2023-05-13 16:51:07 +0000

    Deploying to gh-pages from @ S4ter/goit-js-hw-09@9b3b82916f8ac90010f2bc4ca10ea8765d828fb0 🚀

--- 01-color-switcher.f59e9488.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");var a=null;t.addEventListener("click",(function(){a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(a),t.removeAttribute("disabled","disabled")}))}();
-//# sourceMappingURL=01-color-switcher.f59e9488.js.map
